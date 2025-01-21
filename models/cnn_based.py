f7377c05176253c6a707556515c4b5a834f545a3
SirAfshin <aabb31531@gmail.com>
2025-01-21 10:39:41 +0330

    Update: add attention to TSception

--- models/cnn_based.py
+++ models/cnn_based.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn 
@@ -68,13 +69,119 @@ class TSception(nn.Module):
         out = self.fc(out)
         return out
 
+class MultiHeadSelfAttention_EEG(nn.Module):
+    def __init__(self, input_dim, d_model, seq_length, num_heads):
+        '''
+        No mask is use in this Scaled Dot Product design !
+        input_dim = time_length
+        seq_length = num_channels
+        Note: most of the time d_model = input_dim
+        '''
+        super(MultiHeadSelfAttention_EEG, self).__init__()
+        self.input_dim = input_dim
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.head_dim = d_model // num_heads
+        self.qkv_layer = nn.Linear(input_dim , 3 * d_model)
+        self.linear_layer = nn.Linear(d_model, d_model)
+        self.layer_norm = nn.LayerNorm(d_model)
+   
+    def scaled_dot_product(self, q, k, v, mask=None):
+        d_k = q.size()[-1]
+        scaled = torch.matmul(q, k.transpose(-1, -2)) / math.sqrt(d_k)
+        if mask is not None:
+            scaled += mask
+        attention = F.softmax(scaled, dim=-1)
+        values = torch.matmul(attention, v)
+        return values, attention
+        
+    def forward(self,x):
+        x = x.squeeze(1)
+        batch_size, sequence_length, input_dim = x.size()
+        qkv = self.qkv_layer(x)
+        qkv = qkv.reshape(batch_size, sequence_length, self.num_heads, 3 * self.head_dim)
+        qkv = qkv.permute(0, 2, 1, 3) # (b, head, c, features)
+        q, k, v = qkv.chunk(3, dim=-1) # Extract Q K V
+        values, attention = self.scaled_dot_product(q, k, v, None)
+        values = values.reshape(batch_size, sequence_length, self.num_heads * self.head_dim)
+        out = self.linear_layer(values)
+        out = self.layer_norm(out + x)  # Add & Norm
+        
+        # plt.imshow(attention[0][5].detach().numpy(), cmap= 'autumn')
+        # plt.colorbar() 
+        # plt.show()
+        
+        return out
+
+class TSceptionATN(nn.Module):
+    def conv_block(self, in_chan, out_chan, kernel, step, pool):
+        return nn.Sequential(
+            nn.Conv2d(in_channels=in_chan, out_channels=out_chan,
+                      kernel_size=kernel, stride=step),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(kernel_size=(1, pool), stride=(1, pool)))
+
+    def __init__(self, num_classes, input_size, sampling_rate, num_T, num_S, hidden, dropout_rate):
+        # input_size: 1 x EEG channel x datapoint
+        super(TSceptionATN, self).__init__()
+        self.inception_window = [0.5, 0.25, 0.125]
+        self.pool = 8
+
+        # Multi Head Self Attention
+        self.multi_head_self_attention = MultiHeadSelfAttention_EEG(input_size[-1], input_size[-1], input_size[1],num_heads=8)
+
+        # by setting the convolutional kernel being (1,lenght) and the strids being 1 we can use conv2d to
+        # achieve the 1d convolution operation
+        self.Tception1 = self.conv_block(1, num_T, (1, int(self.inception_window[0] * sampling_rate)), 1, self.pool)
+        self.Tception2 = self.conv_block(1, num_T, (1, int(self.inception_window[1] * sampling_rate)), 1, self.pool)
+        self.Tception3 = self.conv_block(1, num_T, (1, int(self.inception_window[2] * sampling_rate)), 1, self.pool)
+
+        self.Sception1 = self.conv_block(num_T, num_S, (int(input_size[1]), 1), 1, int(self.pool * 0.25))
+        self.Sception2 = self.conv_block(num_T, num_S, (int(input_size[1] * 0.5), 1), (int(input_size[1] * 0.5), 1),
+                                         int(self.pool * 0.25))
+        self.fusion_layer = self.conv_block(num_S, num_S, (3, 1), 1, 4)
+        self.BN_t = nn.BatchNorm2d(num_T)
+        self.BN_s = nn.BatchNorm2d(num_S)
+        self.BN_fusion = nn.BatchNorm2d(num_S)
+
+        self.fc = nn.Sequential(
+            nn.Linear(num_S, hidden),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.multi_head_self_attention(x)
+        x = x.unsqueeze(1)
+        y = self.Tception1(x)
+        out = y
+        y = self.Tception2(x)
+        out = torch.cat((out, y), dim=-1)
+        y = self.Tception3(x)
+        out = torch.cat((out, y), dim=-1)
+        out = self.BN_t(out)
+        z = self.Sception1(out)
+        out_ = z
+        z = self.Sception2(out)
+        out_ = torch.cat((out_, z), dim=2)
+        out = self.BN_s(out_)
+        out = self.fusion_layer(out)
+        out = self.BN_fusion(out)
+        out = torch.squeeze(torch.mean(out, dim=-1), dim=-1)
+        out = self.fc(out)
+        return out
 
 
 
 
 
+if __name__ == "__main__":
+    x = torch.rand(10,1,14,128)
+    mhsa = MultiHeadSelfAttention_EEG(128,128,14,8)
+    print(mhsa(x).shape)
 
-
-
+    model = TSceptionATN(2,[1,14,128],128,32,32,64,0.3)   
+    print(model(x).shape)
 
 
