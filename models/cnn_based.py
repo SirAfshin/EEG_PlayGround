e3d5e142b3e94ad154dd6ab79a65370f149814ff
SirAfshin <aabb31531@gmail.com>
2025-01-27 20:41:57 +0330

    Update: add Unet and Unet_Vit Model

--- models/cnn_based.py
+++ models/cnn_based.py
@@ -18,7 +18,9 @@ from torch.nn.parameter import Parameter
 
 from einops.layers.torch import Rearrange
 
-from utils.utils import get_num_trainable_params
+from utils.utils import get_num_trainable_params, resize_tensor
+from models.Transformer import VisionTransformerEEG
+
 
 # Activation Function dictionary
 nonlinearity_dict = dict(relu=nn.ReLU(), elu=nn.ELU())
@@ -182,6 +184,93 @@ class TSceptionATN(nn.Module):
         out = self.fc(out)
         return out
 
+# U-Net
+# [https://www.youtube.com/watch?v=IHq1t7NxS8k]
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False), # bias=False in order to use batch norm
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False), # bias=False in order to use batch norm
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),            
+        )
+    
+    def forward(self, x):
+        return self.conv(x)
+
+class UNET(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1, feature_channels=[64,128,256,512]):
+        super().__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Down part of Unet
+        for feature in feature_channels:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+        
+        # Up part of Unet
+        for feature in reversed(feature_channels):
+            self.ups.append(
+                nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2), # *2 is for the concatanation
+            ) # kernel=2 strid=2 -> doubles the height and width of image
+            self.ups.append(DoubleConv(feature*2, feature))
+
+        self.bottleneck = DoubleConv(feature_channels[-1], feature_channels[-1]*2)
+        self.final_conv = nn.Conv2d(feature_channels[0], out_channels, kernel_size=1)
+
+    def forward(self,x):
+        skip_connections=[]
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+
+        skip_connections = skip_connections[::-1] # reverse list of skip connections for the decoder part (up)
+
+        for idx in range(0, len(self.ups), 2): # each up and double conv is a single step
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx//2]
+
+            if x.shape != skip_connection.shape:
+                x = resize_tensor(x, size=skip_connection.shape[2:])
+
+            concat_skip = torch.cat((skip_connection, x), dim=1) # concatenate along channel dimension (b,channel,h,w)
+            x = self.ups[idx+1](concat_skip)
+
+        x = self.final_conv(x)
+        
+        return x
+
+class UNET_VIT(nn.Module):
+    def __init__(self,in_channels=128,unet_out_channels=3,img_size=9, patch_size=3, 
+    n_classes=2,embed_dim=768,depth=5, n_heads=6,mlp_ratio=4.,qkv_bias=True,p=0.5,attn_p=0.5,):
+        super().__init__()
+        self.unet = UNET(in_channels=in_channels,out_channels=unet_out_channels, feature_channels=[64,128,256])
+        self.vit = VisionTransformerEEG(
+            img_size= img_size, # data[3,9,9]
+            patch_size=patch_size,
+            in_chans=unet_out_channels,
+            n_classes=n_classes,
+            embed_dim=embed_dim,
+            depth=depth,
+            n_heads=n_heads,
+            mlp_ratio=mlp_ratio,
+            qkv_bias=qkv_bias,
+            p=p,
+            attn_p=attn_p,)
+    
+    def forward(self,x):
+        x = self.unet(x)
+        x = self.vit(x)
+        return x
 
 
 
@@ -190,8 +279,27 @@ if __name__ == "__main__":
     x = torch.rand(10,1,14,128)
     mhsa = MultiHeadSelfAttention_EEG(128,128,14,8)
     print(mhsa(x).shape)
+    #########################################################################
 
     model = TSceptionATN(2,[1,14,128],128,32,32,64,0.3)   
     print(model(x).shape)
-
     print(get_num_trainable_params(model,1))
+    #########################################################################
+    
+    print('*'*20)
+    x = torch.rand(10,128,9,9)
+    model = UNET(in_channels=x.shape[1],out_channels=1, feature_channels=[64,128,256])
+    print(get_num_trainable_params(model,1))
+    print(f"[UNET] original input: {x.shape}, output: {model(x).shape}")
+    #########################################################################
+
+    print('*'*20)
+    x = torch.rand(10,128,9,9)
+    model = UNET_VIT(
+    in_channels=128,unet_out_channels=3,img_size=9, patch_size=3, 
+    n_classes=2,embed_dim=768,depth=5, n_heads=6,mlp_ratio=4.,qkv_bias=True,p=0.5,attn_p=0.5
+    )
+    print(get_num_trainable_params(model,1))
+    print(f"[UNET_VIT] original input: {x.shape}, output: {model(x).shape}")
+    #########################################################################
+
