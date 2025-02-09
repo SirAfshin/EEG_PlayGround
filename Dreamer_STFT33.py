1b3df0168180359e33951334449780e720aa105e
SirAfshin <aabb31531@gmail.com>
2025-02-09 14:38:29 +0330

    UpdateL dreamer stft 33

--- Dreamer_STFT33.py
+++ Dreamer_STFT33.py
@@ -153,7 +153,7 @@ if __name__ == "__main__":
 
     # model = UNET_DGCNN_INCEPTION_GAT(in_channels=dataset[0][0].shape[0], unet_feature_channels=[64,128,256], graph_feature_size=5, dgcnn_layers=2, dgcnn_hid_channels=32, n_classes=2, dropout=0.5, bias=True)
     
-    # val: 78%
+    # val: 80%
     # model = NO_UNET_DGCNN_INCEPTION_GAT(in_channels=dataset[0][0].shape[0], unet_feature_channels=[64,128,256], graph_feature_size=5, n_classes=2,linear_hid=64)
     
     model = UNET_DGCNN_INCEPTION_GAT_Multi_Head(
