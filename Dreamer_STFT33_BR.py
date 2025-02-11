af65a22c4049994583f09d435567b84f04ff4563
SirAfshin <aabb31531@gmail.com>
2025-02-11 13:51:41 +0330

    update: epoch size

--- Dreamer_STFT33_BR.py
+++ Dreamer_STFT33_BR.py
@@ -189,7 +189,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
     
-    num_epochs = 200 # 300 500 600 800
+    num_epochs = 40 # 300 500 600 800
     model_name = dataset_name + "_" + model.__class__.__name__  
 
     print(f"Start training for {num_epochs} epoch")
